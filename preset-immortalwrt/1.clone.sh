014950a4c7a51f4e9ac97501ba2a0d04bacddf39
Pinkman:) <43259075+zhuang-zx2018@users.noreply.github.com>
2025-05-27 12:31:20 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -29,3 +29,4 @@ git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
+git clone --depth 1 https://github.com/zhuang-zx2018/UA2F.git
