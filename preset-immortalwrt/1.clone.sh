bf468b1f0186c92e956ab120b6ca40d776cf67c4
Pinkman:) <43259075+zhuang-zx2018@users.noreply.github.com>
2025-05-29 20:06:33 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -24,6 +24,7 @@ mv ./$CODE_DIR/* ./
 SUPPLY_DIR=_supply_packages
 echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
 mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/zhuang-zx2018/UA2F.git
