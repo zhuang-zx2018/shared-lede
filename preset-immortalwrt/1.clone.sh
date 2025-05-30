b563af88ed2969df2a843ddb5e511fefdc46ee84
Pinkman:) <43259075+zhuang-zx2018@users.noreply.github.com>
2025-05-30 18:25:46 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -8,6 +8,7 @@ SWITCH_LATEST_TAG=false
 git clone --single-branch -b $CODE_BRANCH $CODE_URL $CODE_DIR
 if $SWITCH_LATEST_TAG; then
     cd $CODE_DIR
+    curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
     LATEST_TAG_HASH=$(git rev-list --tags --max-count=1)
     if [ -z "$LATEST_TAG_HASH" ]; then
         echo "No tag to switch, keep the latest commit"
@@ -19,12 +20,10 @@ if $SWITCH_LATEST_TAG; then
     cd ..
 fi
 mv ./$CODE_DIR/* ./
-
 # download app codes
 SUPPLY_DIR=_supply_packages
 echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
 mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/zhuang-zx2018/UA2F.git
