1480283ce81a2f1f79e1b36da2289a3b163a6984
Pinkman:) <43259075+zhuang-zx2018@users.noreply.github.com>
2025-06-01 01:35:55 +0800

    Update 1.clone.sh

--- preset-lede/1.clone.sh
+++ preset-lede/1.clone.sh
@@ -5,10 +5,15 @@ CODE_DIR=_firmware_code
 git clone --depth 1 https://github.com/coolsnowwolf/lede.git $CODE_DIR
 mv ./$CODE_DIR/* ./
 
+cd $CODE_DIR
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
 # download app codes
 SUPPLY_DIR=_supply_packages
 echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
 mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages.git
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone --depth 1 https://github.com/zhuang-zx2018/UA2F.git
